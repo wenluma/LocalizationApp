8bf7dadd747ad81de6de11dcc8e9e3d7fa346070
苗高亮 <miaogaoliang@kuark.com>
2015-03-06 16:26:21 +0800

    Initial Commit

---
+++ LocalizationApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocalizationApp
+//
+//  Created by kaxiaoer on 15/3/6.
+//  Copyright (c) 2015年 miaogaoliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocalizationApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalizationApp
+//
+//  Created by kaxiaoer on 15/3/6.
+//  Copyright (c) 2015年 miaogaoliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
